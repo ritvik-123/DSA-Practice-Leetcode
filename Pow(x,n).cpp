8dcd39b352ff6180ad235bb82ebca041ebdfb614
Ritvik Mahapatra <ritvik.mahapatra@yahoo.com>
2026-09-09 18:18:31 -0700

    P50: Pow(x,n)

---
+++ Pow(x,n).cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n;
+        if(N<0)
+        {
+            x = 1/x;
+            N = -N;
+        }
+        if(N==0)
+        {
+            return 1;
+        }
+        if(N%2 == 0)
+        {
+            double half = myPow(x,N/2);
+            return half*half;
+        }
+        return x*myPow(x,N-1);
+    }
+};
